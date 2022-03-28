35d1e78e8dc60b107248fe2c517b1b97bd2100ea
Rebcsi <kovacsr.12d@acsjszki.hu>
2022-03-28 13:12:39 +0200

    3fel

--- 2022-03-28_Balkezesek/Program.cs
+++ 2022-03-28_Balkezesek/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _2022_03_28_Balkezesek
 {
@@ -10,6 +11,14 @@ namespace _2022_03_28_Balkezesek
     {
         static void Main(string[] args)
         {
+            List<sportoló> sportolók = new List<sportoló>();
+            foreach (var sor in File.ReadAllLines("balkezesek.csv").Skip(1))
+            {
+                sportolók.Add(new sportoló(sor));
+            }
+            Console.WriteLine($"3.feladat: {sportoló.Count}");
+
+            Console.ReadKey();
         }
     }
 }
